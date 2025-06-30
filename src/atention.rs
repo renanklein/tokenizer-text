acfe636989912562b02a9776327b5f0bfb232c2f
renanklein <renankleinshow@gmail.com>
2025-06-30 09:45:28 -0300

    Adds train method

--- src/atention.rs
+++ src/atention.rs
@@ -1,8 +1,7 @@
 use std::fmt::Debug;
 
 use tch::{
-    nn::{self, Module, VarStore},
-    Device, Tensor,
+    nn::{self, Module, ModuleT, VarStore}, Device, Kind,Tensor
 };
 
 #[derive(Debug)]
@@ -10,14 +9,38 @@ pub struct Attention {
     query_linear: nn::Linear,
     key_linear: nn::Linear,
     value_linear: nn::Linear,
+    out_proj: nn::Linear,
+    mask_init: Tensor,
     d_out: i64,
     num_head: i64,
     head_dim: i64
 }
 
+impl Attention {
+    pub fn new(d_in: i64, d_out: i64, num_head: i64, context_length: i64) -> Self {
+        let vs = VarStore::new(Device::cuda_if_available());
+        let root = vs.root();
 
-impl Module for Attention {
-    fn forward(&self, input: &Tensor) -> Tensor {
+        let query_linear = nn::linear(&root, d_in, d_out, Default::default());
+        let key_linear = nn::linear(&root, d_in, d_out, Default::default());
+        let value_linear = nn::linear(&root, d_in, d_out, Default::default());
+        let out_proj = nn::linear(&root, d_in, d_out, Default::default());
+        let head_dim = d_out/num_head as i64;
+        let mask_init = Tensor::ones([context_length, context_length], (Kind::Float, Device::cuda_if_available())).tril(1);
+
+        Attention {
+            query_linear,
+            key_linear,
+            value_linear,
+            out_proj,
+            mask_init,
+            d_out,
+            num_head,
+            head_dim
+        }
+    }
+
+    pub fn forward(&self, input: &Tensor) -> Tensor {
         let queries = self.query_linear.forward(input);
         let keys = self.key_linear.forward(input);
         let values = self.value_linear.forward(input);
@@ -30,21 +53,16 @@ impl Module for Attention {
 
         attn_weights
     }
-}
 
-impl Attention {
-    pub fn new(d_in: i64, d_out: i64) -> Self {
-        let vs = VarStore::new(Device::cuda_if_available());
-        let root = vs.root();
-
-        let query_linear = nn::linear(&root, d_in, d_out, Default::default());
-        let key_linear = nn::linear(&root, d_in, d_out, Default::default());
-        let value_linear = nn::linear(&root, d_in, d_out, Default::default());
-
-        Attention {
-            query_linear,
-            key_linear,
-            value_linear,
-        }
+    pub fn train(&self, input: Tensor) -> impl ModuleT {
+        nn::func_t(move |input, train| {
+            let (sz_b, sz_t, sz_c) = input.size3().unwrap();
+            let sizes = [sz_b, sz_t, self.num_head, sz_c / self.num_head];
+            let k = input.apply(&self.key_linear).view(sizes).transpose(1, 2);
+            let q = input.apply(&self.query_linear).view(sizes).transpose(1, 2);
+            let v = input.apply(&self.value_linear).view(sizes).transpose(1, 2);
+            let att_scores = q.matmul(&k.transpose(-2, -1) * (1.0 / f64::sqrt(sizes[3] as f64)));
+        })
     }
+
 }
