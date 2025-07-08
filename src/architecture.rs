cb8cda34d490cadfbab39ac0b63a4b1b72f09189
renanklein <renankleinshow@gmail.com>
2025-07-08 09:42:03 -0300

    Adds FeedForward struct

--- src/architecture.rs
+++ src/architecture.rs
@@ -1,14 +1,23 @@
 use std::f64::consts::PI;
 
-use tch::{nn::{self, VarStore}, Device, Kind, Tensor};
+use tch::{nn::{self, linear, seq, Linear, Sequential, VarStore}, Device, Kind, Tensor};
 
+#[derive(Debug)]
 pub struct LayerNorm {
     eps: f64,
     shift: Tensor,
     scale: Tensor,
 }
 
+
+#[derive(Debug)]
 pub struct GELU;
+
+#[derive(Debug)]
+pub struct FeedForward {
+    emb_dim: i64,
+    layers: Sequential
+}
 
 impl LayerNorm {
     pub fn new(emb_dim: i64) -> Self {
@@ -23,14 +32,18 @@ impl LayerNorm {
         LayerNorm { eps, shift, scale }
     }
 
-    pub fn forward(&self, x: Tensor) -> Tensor {
-        let mean = x.mean_dim(Some(&[-1i64][..]), true, Kind::Float);
-        let var = x.var_dim(Some(&[-1i64][..]), false, true);
+}
 
-        let norm_x = (x - mean)/Tensor::sqrt(&(self.eps + var));
+impl nn::Module for LayerNorm {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        let mean = xs.mean_dim(Some(&[-1i64][..]), true, Kind::Float);
+        let var = xs.var_dim(Some(&[-1i64][..]), false, true);
+
+        let norm_x = (xs - mean)/Tensor::sqrt(&(self.eps + var));
 
         &self.scale * norm_x + &self.shift
     }
+
 }
 
 impl GELU {
@@ -38,9 +51,34 @@ impl GELU {
         GELU
     }
 
-    pub fn foward(&self, x: Tensor) -> Tensor{
+}
+
+impl nn::Module for GELU {
+
+    fn forward(&self, xs: &Tensor) -> Tensor{
         let part = Tensor::from(2.0 / PI);
 
-        0.5 * &x * (1.0 + Tensor::tanh(&(&part * &(&x + 0.044715 + x.pow_tensor_scalar(3.0)))))
+        0.5 * xs * (1.0 + Tensor::tanh(&(&part * &(xs + 0.044715 + xs.pow_tensor_scalar(3.0)))))
+
     }
 }
+
+impl FeedForward {
+    pub fn new(emb_dim: i64) -> Self {
+        let vs = VarStore::new(Device::cuda_if_available());
+        let layer1 = linear(vs.root(), emb_dim, 4 * emb_dim, Default::default());
+        let layer2 = linear(vs.root(), 4 * emb_dim, emb_dim, Default::default());
+
+        let layers = seq().add(layer1).add(GELU::new()).add(layer2);
+
+        FeedForward { emb_dim, layers }
+    }
+
+}
+
+
+//impl nn::Module for FeedForward {
+  //  fn forward(&self, xs: &Tensor) -> Tensor {
+        
+    //}
+//}
