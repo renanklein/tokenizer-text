507b65bfe09f5299b280f57ca7bca2c018c42813
renanklein <renankleinshow@gmail.com>
2025-08-04 09:43:30 -0300

    Adds generate text method

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,9 @@
-use tch::{nn::{embedding, linear, seq, Embedding, EmbeddingConfig, Linear, Module, Sequential, VarStore}, Device, Kind, Shape, Tensor};
+use tch::{
+    nn::{
+        embedding, linear, seq, Embedding, EmbeddingConfig, Linear, Module, Sequential, VarStore,
+    },
+    Device, IndexOp, Kind, Shape, Tensor,
+};
 
 use crate::{architecture::LayerNorm, config::Config, transformer::Transformer};
 
@@ -9,17 +14,26 @@ pub struct Model {
     trf_blocks: Sequential,
     final_norm: LayerNorm,
     out_head: Linear,
-    drop_rate: f64
+    drop_rate: f64,
 }
-
 
 impl Model {
     pub fn new(cfg: Config) -> Self {
         let vs = VarStore::new(Device::cuda_if_available());
         let root = vs.root();
 
-        let token_emb = embedding(&root/"token_emb", cfg.vocab_size, cfg.emb_dim, Default::default());
-        let pos_emb = embedding(&root/"pos_emb", cfg.context_length, cfg.emb_dim, Default::default());
+        let token_emb = embedding(
+            &root / "token_emb",
+            cfg.vocab_size,
+            cfg.emb_dim,
+            Default::default(),
+        );
+        let pos_emb = embedding(
+            &root / "pos_emb",
+            cfg.context_length,
+            cfg.emb_dim,
+            Default::default(),
+        );
 
         let mut trf_blocks = seq();
 
@@ -30,9 +44,36 @@ impl Model {
 
         let final_norm = LayerNorm::new(cfg.emb_dim);
 
-        let out_head = linear(&root/"head", cfg.emb_dim, cfg.vocab_size, Default::default());
+        let out_head = linear(
+            &root / "head",
+            cfg.emb_dim,
+            cfg.vocab_size,
+            Default::default(),
+        );
 
-        Model { token_emb, pos_emb, trf_blocks , final_norm, out_head, drop_rate: cfg.drop_rate }
+        Model {
+            token_emb,
+            pos_emb,
+            trf_blocks,
+            final_norm,
+            out_head,
+            drop_rate: cfg.drop_rate,
+        }
+    }
+
+    pub fn generate_text(self, input: Tensor, max_new_tokens: i64, context_size: i64) -> Tensor {
+        let mut current = input.copy();
+
+        let next_token =  tch::no_grad(|| {
+            let current_cond = current.slice(-1, -context_size, i64::MAX, 1);
+            for _ in 0..max_new_tokens {
+                let logits = current_cond .apply(&self).i((0, -1, ..));
+                let probas = logits.softmax(-1, Kind::Float);
+                probas.argmax(-1, true)
+            }
+        });
+
+        Tensor::cat(&[current, next_token], 1)
     }
 }
 
@@ -40,7 +81,9 @@ impl Module for Model {
     fn forward(&self, xs: &Tensor) -> Tensor {
         let (_, seq_length) = xs.size2().unwrap();
         let token_emb = self.token_emb.forward(xs);
-        let pos_emb = self.pos_emb.forward(&Tensor::arange(seq_length, (Kind::Float, xs.device())));
+        let pos_emb = self
+            .pos_emb
+            .forward(&Tensor::arange(seq_length, (Kind::Float, xs.device())));
 
         (token_emb + pos_emb)
             .dropout(self.drop_rate, false)
