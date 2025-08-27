0b8c888a69f4c1e821524cbc75810f4eb7cd00d1
renanklein <renankleinshow@gmail.com>
2025-08-27 09:43:46 -0300

    Adds calc loss loader

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use ai_dataloader::Len;
 use tch::{nn::{self, Module}, Device, Tensor};
 use tiktoken_rs::{get_bpe_from_tokenizer, tokenizer::get_tokenizer};
 
-use crate::{config::Config, model::Model};
+use crate::{config::Config, data_modifier::GPTDatasetV1, model::Model};
 
 mod architecture;
 mod atention;
@@ -39,6 +40,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dataset_train = data_modifier::GPTDatasetV1::new(&tokenizer, train_text, 256, 256);
     let dataset_val  = data_modifier::GPTDatasetV1::new(&tokenizer, val_text, 256, 256);
 
+    let model = Model::new(config);
+    let train_size = dataset_train.input_ids.len();
+
+    call_loss_loader(dataset_train, model, train_size);
     Ok(())
 }
 
@@ -56,3 +61,18 @@ fn calc_loss_batch(input_batch: Tensor, target_batch: Tensor, model: &Model) -> 
 
 }
 
+fn call_loss_loader(dataset: GPTDatasetV1, model: Model, mut num_batch: usize){
+    let mut total_loss = 0.;
+    let dataset_len = dataset.input_ids.len();
+    num_batch = num_batch.min(dataset_len);
+
+    for i in 0..dataset_len {
+        let batch_tuple = dataset.get_item(i);
+
+        if i < num_batch {
+            let loss = calc_loss_batch(batch_tuple.0, batch_tuple.1, &model);
+            loss.print();
+        }
+    }
+
+}
