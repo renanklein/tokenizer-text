d4743f04f4a9080f1854dd2e8e2ddf351caeaf52
renanklein <renankleinshow@gmail.com>
2025-09-03 09:40:21 -0300

    Starts adding training method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ai_dataloader::Len;
-use tch::{nn::{self, Module}, Device, Tensor};
+use tch::{data::TextData, nn::{self, AdamW, Module, Optimizer, OptimizerConfig, VarStore}, Device, Kind, Tensor};
 use tiktoken_rs::{get_bpe_from_tokenizer, tokenizer::get_tokenizer};
 
 use crate::{config::Config, data_modifier::GPTDatasetV1, model::Model};
@@ -12,6 +12,14 @@ mod file_utils;
 mod model;
 mod simple_tokenizer;
 mod transformer;
+
+
+const LEARNING_RATE: f64 = 0.0003;
+const EPOCHS: i64 = 100;
+const NO_WEIGHT_DECAY_GROUP: usize = 0;
+const WEIGHT_DECAY_GROUP: usize = 1;
+const BLOCK_SIZE: i64 = 128;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -55,9 +63,9 @@ async fn get_text_from_file(file_path: &str) -> Result<String, Box<dyn std::erro
     Ok(file_content)
 }
 
-fn calc_loss_batch(input_batch: Tensor, target_batch: Tensor, model: &Model) -> Tensor {
+fn calc_loss_batch(input_batch: &Tensor, target_batch: &Tensor, model: &Model) -> Tensor {
 
-    let logits = model.forward(&input_batch);
+    let logits = model.forward(input_batch);
 
     let targets = target_batch.to_kind(tch::Kind::Int64).flatten(0, 1);
 
@@ -65,16 +73,18 @@ fn calc_loss_batch(input_batch: Tensor, target_batch: Tensor, model: &Model) -> 
 
 }
 
-fn call_loss_loader(dataset: GPTDatasetV1, model: Model, mut num_batch: usize) -> f64{
+fn call_loss_loader(dataset: GPTDatasetV1, model: Model, mut num_batch: f64, mut optimizer: Optimizer) -> f64{
     let mut total_loss = 0.;
     let dataset_len = dataset.input_ids.len();
-    num_batch = num_batch.min(dataset_len);
+    num_batch = num_batch.min(dataset_len as f64);
 
     for i in 0..dataset_len {
         let batch_tuple = dataset.get_item(i);
 
-        if i < num_batch {
+        if i < num_batch as usize {
             let loss = calc_loss_batch(batch_tuple.0, batch_tuple.1, &model);
+
+            optimizer.backward_step_clip(&loss, 0.5);
 
             let loss_value: f64 = loss.try_into().unwrap();
             total_loss += loss_value;
@@ -82,5 +92,28 @@ fn call_loss_loader(dataset: GPTDatasetV1, model: Model, mut num_batch: usize) -
     }
 
     total_loss/(num_batch as f64)
+
+}
+
+fn train (data: TextData, model: Model, device: Device) {
+    let vs = VarStore::new(device);
+    let mut optimizer = AdamW::default().build(&vs, LEARNING_RATE).unwrap();
+
+    optimizer.set_weight_decay_group(WEIGHT_DECAY_GROUP, 0.1);
+    optimizer.set_weight_decay_group(NO_WEIGHT_DECAY_GROUP, 0.0);
+
+    for epoch in 1 .. (1 + EPOCHS) {
+
+        for batch in data.iter_shuffle(BLOCK_SIZE + 1, BLOCK_SIZE) {
+            let input_batch = batch.narrow(1, 0, BLOCK_SIZE).to_kind(Kind::Int64).to_device(device);
+            let target_batch = batch.narrow(1, 1, BLOCK_SIZE).to_kind(Kind::Int64).to_device(device);
+
+            let loss = calc_loss_batch(&input_batch, &target_batch, &model);
+            optimizer.backward_step_clip(&loss, 0.5);
+        }
+
+
+    } 
+
 
 }
