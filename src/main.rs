4d1fae1359d6a2c1939d82e857c2f9d21824e564
renanklein <renankleinshow@gmail.com>
2025-09-05 09:41:07 -0300

    Adds train method

--- src/main.rs
+++ src/main.rs
@@ -45,15 +45,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Err(e) => panic!("Error getting BPE tokenizer: {}", e),
     };
 
+
+    let data = TextData::new("the-verdict.txt")?;
+
+    let model = Model::new(config);
+
+    train(data, model, Device::cuda_if_available());
+
+
+
     let dataset_train = data_modifier::GPTDatasetV1::new(&tokenizer, train_text, 256, 256);
     let dataset_val  = data_modifier::GPTDatasetV1::new(&tokenizer, val_text, 256, 256);
 
-    let model = Model::new(config);
     let train_size = dataset_train.input_ids.len();
 
-    let total_loss = call_loss_loader(dataset_train, model, train_size);
 
-    println!("Total loss entropy {:?}", total_loss);
+
     Ok(())
 }
 
@@ -69,47 +76,47 @@ fn calc_loss_batch(input_batch: &Tensor, target_batch: &Tensor, model: &Model) -
 
     let targets = target_batch.to_kind(tch::Kind::Int64).flatten(0, 1);
 
+
     logits.flatten(0, 1).cross_entropy_for_logits(&targets)
 
 }
 
-fn call_loss_loader(dataset: GPTDatasetV1, model: Model, mut num_batch: f64, mut optimizer: Optimizer) -> f64{
-    let mut total_loss = 0.;
-    let dataset_len = dataset.input_ids.len();
-    num_batch = num_batch.min(dataset_len as f64);
-
-    for i in 0..dataset_len {
-        let batch_tuple = dataset.get_item(i);
-
-        if i < num_batch as usize {
-            let loss = calc_loss_batch(batch_tuple.0, batch_tuple.1, &model);
-
-            optimizer.backward_step_clip(&loss, 0.5);
-
-            let loss_value: f64 = loss.try_into().unwrap();
-            total_loss += loss_value;
-        }
-    }
-
-    total_loss/(num_batch as f64)
-
-}
 
 fn train (data: TextData, model: Model, device: Device) {
     let vs = VarStore::new(device);
     let mut optimizer = AdamW::default().build(&vs, LEARNING_RATE).unwrap();
 
-    optimizer.set_weight_decay_group(WEIGHT_DECAY_GROUP, 0.1);
-    optimizer.set_weight_decay_group(NO_WEIGHT_DECAY_GROUP, 0.0);
+    //optimizer.set_weight_decay_group(WEIGHT_DECAY_GROUP, 0.1);
+    //optimizer.set_weight_decay_group(NO_WEIGHT_DECAY_GROUP, 0.0);
+
+
+    let mut idx = 0;
 
     for epoch in 1 .. (1 + EPOCHS) {
 
+        let mut sum_loss = 0.;
+        let mut cnt_loss = 0.;
+
         for batch in data.iter_shuffle(BLOCK_SIZE + 1, BLOCK_SIZE) {
+
             let input_batch = batch.narrow(1, 0, BLOCK_SIZE).to_kind(Kind::Int64).to_device(device);
             let target_batch = batch.narrow(1, 1, BLOCK_SIZE).to_kind(Kind::Int64).to_device(device);
 
             let loss = calc_loss_batch(&input_batch, &target_batch, &model);
+
+            loss.print();
             optimizer.backward_step_clip(&loss, 0.5);
+
+            sum_loss += f64::try_from(loss).unwrap();
+            cnt_loss += 1.0;
+            idx += 1;
+
+            if idx % 10000 == 0 {
+                println!("Epoch : {:?} and loss {:5.3}", epoch, sum_loss/cnt_loss);
+                
+            }
+
+
         }
 
 
